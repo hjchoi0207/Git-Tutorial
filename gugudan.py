368e663aeed3208ebe8730ccb3b0c83361cb82a4
hjchoi0207 <chjin95@gmail.com>
2020-08-24 22:03:35 +0900

    Add Py File [gugudan.py]

---
+++ gugudan.py
@@ -0,0 +1,14 @@
+#GuGu
+
+def gugu(x):
+    gugudan = []
+    for i in range(1,10):
+        res = i * x
+        gugudan.append(res)
+
+    print(gugudan)
+    #for i in range(9):
+    #   print(gugudan[i], end = ' ')
+
+
+gugu(2)
